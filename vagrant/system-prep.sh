7f45fa282cf76e7370d70722e4e1b08748559ae6
Maik Glatki <maik@glatki.eu>
2016-11-14 13:12:11 +0000

    Add jq to installed software.

--- vagrant/system-prep.sh
+++ vagrant/system-prep.sh
@@ -6,6 +6,6 @@ sed -i 's/allowed_users=.*$/allowed_users=anybody/' /etc/X11/Xwrapper.config
 ## Other programs
 apt-get install -y git vim htop xterm chromium dos2unix
 
-# Some of my more personal stuff. Comment it out if you do not like it
-apt-get install -y zsh tree tmux curl
+# Some of my more personal preferred software. Comment it out if you do not like it
+apt-get install -y zsh tree tmux curl jq
 chsh vagrant -s /bin/zsh
