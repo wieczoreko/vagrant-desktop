e31de108c7e51ab5d2c81bd7d2a5a9c102f46076
Maik Glatki <maik@glatki.eu>
2016-11-18 13:28:57 +0000

    Install awscli tools.

--- vagrant/system-prep.sh
+++ vagrant/system-prep.sh
@@ -7,7 +7,7 @@ sed -i 's/allowed_users=.*$/allowed_users=anybody/' /etc/X11/Xwrapper.config
 apt-get install -y git vim htop xterm chromium dos2unix
 
 # Some of my more personal preferred software. Comment it out if you do not like it
-apt-get install -y zsh tree tmux curl jq s3cmd vim-nox
+apt-get install -y zsh tree tmux curl jq s3cmd vim-nox awscli
 # Some libraries I often use
 apt-get install -y libwww-perl libjson-perl
 
