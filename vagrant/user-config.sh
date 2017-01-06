011393f21212264eab9711291d042a5e50456ed3
Oktawiusz Wieczorek <oktawiusz.wieczorek@cbc.de>
2017-01-06 09:35:48 +0000

    changed dotfiles repository

--- vagrant/user-config.sh
+++ vagrant/user-config.sh
@@ -2,7 +2,7 @@
 # Configure vagrant user environment
 
 # Install my personal dotfiles. Take it or leave it.
-git clone https://github.com/mgla/dotfiles /home/vagrant/dotfiles && /home/vagrant/dotfiles/deploy
+git clone https://github.com/wieczoreko/dotfiles /home/vagrant/dotfiles && /home/vagrant/dotfiles/deploy
 
 # Install .xinitrc from vagrant-desktop sync.
 ln -s /home/vagrant/vagrant-desktop/vagrant/xinitrc /home/vagrant/.xinitrc
