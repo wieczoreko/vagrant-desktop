62ca0cd23639707b2a58e65a5c9ed41b1bcc2f78
Oktawiusz Wieczorek <oktawiusz.wieczorek@cbc.de>
2017-09-15 14:32:20 +0200

    deleted not used programms

--- vagrant/system-prep.sh
+++ vagrant/system-prep.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 apt-get update
-apt-get install -y fluxbox virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11 x11-xserver-utils xinit lightdm
+apt-get install -y virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11 x11-xserver-utils xinit
 sed -i 's/allowed_users=.*$/allowed_users=anybody/' /etc/X11/Xwrapper.config
 
 ## Other programs
-apt-get install -y git vim htop xterm chromium dos2unix
+apt-get install -y git vim htop chromium dos2unix
 
 # Some of my more personal preferred software. Comment it out if you do not like it
-apt-get install -y zsh tree tmux curl jq s3cmd vim-nox awscli perl-doc
-# Some libraries I often use
-apt-get install -y libwww-perl libjson-perl
+apt-get install -y zsh tree curl jq s3cmd vim-nox awscli
 
 chsh vagrant -s /bin/zsh
-
-# Configure X
-mkdir -p /etc/lightdm/lightdm.conf.d
-install -o root -g root -m 644 /home/vagrant/vagrant-desktop/vagrant/99-lightdm-autologin-vagrant.conf /etc/lightdm/lightdm.conf.d/99-lightdm-autologin-vagrant.conf
